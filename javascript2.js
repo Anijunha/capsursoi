eecffbbc359100382c67920764780b73b800c8ce
Anijunha <anijunha@gmail.com>
2025-12-11 14:24:49 +0100

    'zf

--- javascript2.js
+++ javascript2.js
@@ -1,1 +1,21 @@
 console.log("Page d'accueil de Louis Chaussignant chargée.");
+const modal = document.getElementById("contactModal");
+const btn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".modal .close");
+
+// Ouvrir la modal au clic
+btn.onclick = () => {
+  modal.classList.add("show");
+}
+
+// Fermer avec la croix
+closeBtn.onclick = () => {
+  modal.classList.remove("show");
+}
+
+// Fermer en cliquant en dehors
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("show");
+  }
+}
